b615687e3cbe558e7a7eeeaae9a4e2c313d6c436
flebedev99 <109581129+flebedev99@users.noreply.github.com>
2022-09-06 15:51:21 +0930

    Update index.js

--- src/index.js
+++ src/index.js
@@ -28,10 +28,6 @@ function searchURL() {
   } else {
     content.src = "https://" + search.value;
   }
-  if (search.value.includes("youpic.com") {
-      search.value = "Don't search for innapropriate things!"
-      window.close();
-}
   if (!search.value.includes(".")) {
     message.innerText = "Bowser browser dosen't own any search engine";
     search.value = "Please type in a valid domain";
